45b4455926cb85f7282751184f68109d970582ba
Usevalad Prastsiakou <folgo1006@gmail.com>
2024-03-11 16:20:38 +0300

    Final testing

--- LR3/Encrypter.java
+++ LR3/Encrypter.java
@@ -2,7 +2,7 @@ package LR3;
 
 public class Encrypter {
     static long convert(long number) {
-        long mask = 0b01010101010101010101010101010101;
+        long mask = 0b01010101_01010101_01010101_01010101;
         return (long)((~number) ^ mask);
     }
 };
